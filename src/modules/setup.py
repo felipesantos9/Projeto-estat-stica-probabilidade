26c62dcd1f76b3498facf5a8a60e5ed9c7c83d25
Felipe Santos <felipessantos2004@gmail.com>
2024-03-20 09:38:42 -0300

    medidas centrais

--- src/modules/setup.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-def install_dependencies():
-    subprocess.run(['pip', 'install', 'numpy'])
-    subprocess.run(['pip', 'install', 'scipy'])
-    subprocess.run(['pip', 'install', 'matplotlib'])
-    subprocess.run(['pip', 'install', 'seaborn'])
